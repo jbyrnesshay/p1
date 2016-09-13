4a7f03ba7b4f0afbd42d2d2db44509eb3af14076
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-13 17:35:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# P1:  first project for DWA15
+
+## Live URL
+<http://p1.midnightoil.me>
+
+## Description
+2-3 sentences here A portal for DWA15 semester projects
+
+## Screencast link
+<http://somelink.com>
+
+## Details for instructores
+No login required
+
+## outside code
+maybe I bootstrap?
+
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> P1: portal for DWA projects </title>
+  <meta charset="utf-8">
+  <link href="css\styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <?php 
+    echo "testing php"."<br>";
+  ?>
+	Joachim Byrnes-Shay
+	csci e-15 dynamic web applications 
+
+	my picture
+
+	project 2
+	live URL
+	github
+
+	project 3
+	live URL
+	github
+
+	project 4
+	live URL
+	github
+
+
+  </body>
+</html>
