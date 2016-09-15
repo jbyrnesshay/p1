9f6439f6569b09e9ed6e2b1c6586989a24603bac
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-14 22:48:27 -0400

    working on columns

--- index.php
+++ index.php
@@ -1,48 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title> P1: portal for DWA projects </title>
-  <meta charset="utf-8">
-  <link href="css/styles.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-	<header>
+  <head>
+    <title> P1: portal for DWA projects </title>
+    <meta charset="utf-8">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <header>
 	   <img src="img/transp.png" alt="joachim">
 	</header>
-	
-        <main>
+	<main>
 	  <div class="top">
-	   
 	    <article id="name" class="left" >
-	     <h1> Joachim Byrnes-Shay</h1>
+	      <h1> Joachim Byrnes-Shay</h1>
 	      <hr>
-	      <h2>csci e-15 projects</h2> 
+	        <h2>csci e-15 projects</h2> 
 	    </article>
-		
-	    <article class="right"  >
-              <h3>project 2</h3>
-	      live: <a href="#"> URL</a> <br>
-	      github: <a href="#"> URL</a>
+		<article class="right"  >
+            <h3>project 2</h3>
+	           live: <a href="#"> URL</a> 
+	           <br>
+	           github: <a href="#"> URL</a>
 	    </article>
-	    
-	   </div>
-
+	  </div>
 	  <div class="bottom">
-	   
 	    <article class="left"   >
 	      <h3>project 3</h3>
-	      live: <a href="#">URL</a> <br>
-	      github: <a href="#">URL</a>
+	        live: <a href="#">URL</a> 
+	        <br>
+	        github: <a href="#">URL</a>
 	    </article>
 	    <article class="right" >
 	      <h3>project 4</h3>
-	      live: <a href="#"> URL</a> <br>
-	      github: <a href="#">URL</a>
+	        live: <a href="#"> URL</a> 
+	        <br>
+	        github: <a href="#">URL</a>
 	    </article>
-
-          </div>
+	  </div>
 	</main>
- 
   </body>
-}
 </html>
