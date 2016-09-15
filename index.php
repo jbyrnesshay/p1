7aa23bafb66501f41568061ffc913b746a75127c
Joachim Byrnes-Shay <jmb464@g.harvard.edu>
2016-09-15 03:10:04 -0400

    fix tag, index.php

--- index.php
+++ index.php
@@ -18,21 +18,16 @@
         <h3>project  2</h3>
 	      live: <a href="#"> URL</a><br>
 	      github: <a href="#"> URL</a>
-	    </div>
 	  </article>
 	  <article>
 	    <h3>project  3</h3>
-	    <section>
 	      live: <a href="#">URL</a><br>
 	      github: <a href="#">URL</a>
-	    </section> 
 	  </article>
 	  <article>
 	    <h3>project  4</h3>
-	    <section>
 	      live: <a href="#"> URL</a><br>
 	      github: <a href="#">URL</a>
-	    </section>
 	  </article>
 	</main>
   </body>
